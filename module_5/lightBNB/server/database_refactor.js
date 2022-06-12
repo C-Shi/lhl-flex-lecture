c533ad217a1fd70dcd1238ff0ae7cbe678506a80
C-Shi <cshi@ualberta.ca>
2022-06-12 00:44:51 -0600

    refactor

--- module_5/lightBNB/server/database_refactor.js
+++ module_5/lightBNB/server/database_refactor.js
@@ -7,22 +7,30 @@ const pool = new Pool({
   host: 'localhost'
 })
 
+function query(qString, qParams) {
+  console.log('Executed Query: ', qString);
+  console.log('Query params: ', qParams);
+  return pool.query(qString, qParams)
+    .then(result => result.rows)
+    .catch(err => console.error(err.message))
+}
+
+function first(data) {
+  return data[0] || null;
+}
+
+function all(data) {
+  return data;
+}
+
 /**
  * Get a single user from the database given their email.
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-  return pool.query('SELECT * FROM users WHERE LOWER(email) = $1', [email.toLowerCase()])
-    .then(result => {
-      if (result.rows.length) {
-        return result.rows[0];
-      }
-      return null
-    })
-    .catch(err => {
-      console.log(err.message)
-    })
+  return query('SELECT * FROM users WHERE LOWER(email) = $1', [email.toLowerCase()])
+    .then(first)
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -32,13 +40,8 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return pool.query('SELECT * FROM users WHERE id = $1', [id])
-    .then(result => {
-      return result.rows[0] || null
-    })
-    .catch(err => {
-      console.log(err.message)
-    })
+  return query('SELECT * FROM users WHERE id = $1', [id])
+    .then(first)
 }
 exports.getUserWithId = getUserWithId;
 
@@ -50,15 +53,10 @@ exports.getUserWithId = getUserWithId;
  */
 const addUser = function (user) {
   const { name, email, password } = user;
-  return pool.query(`
+  return query(`
     INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *;
   `, [name, email, password])
-    .then(result => {
-      return result.rows[0]
-    })
-    .catch(err => {
-      console.log(err.message)
-    })
+    .then(first)
 }
 exports.addUser = addUser;
 
@@ -70,17 +68,11 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return pool.query(`
+  return query(`
     SELECT * FROM reservations JOIN properties ON reservations.property_id = properties.id
     WHERE reservations.guest_id = $1 LIMIT $2;
   `, [guest_id, limit])
-    .then(result => {
-      console.log(result.rows)
-      return result.rows
-    })
-    .catch(err => {
-      console.log(err.message)
-    })
+    .then(all)
 }
 exports.getAllReservations = getAllReservations;
 
@@ -135,13 +127,8 @@ const getAllProperties = function (options, limit = 10) {
   queryString += ` ORDER BY cost_per_night LIMIT $${queryParams.length}`;
   console.log(queryString, queryParams)
 
-  return pool.query(queryString, queryParams)
-    .then(result => {
-      return result.rows
-    })
-    .catch(err => {
-      console.log(err.message)
-    })
+  return query(queryString, queryParams)
+    .then(all)
 }
 exports.getAllProperties = getAllProperties;
 
@@ -154,11 +141,11 @@ exports.getAllProperties = getAllProperties;
 const addProperty = function (property) {
   const { owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms } = property;
 
-  return pool.query(`
+  return query(`
     INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
     VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
     RETURNING *;
   `, [owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms])
-    .then(res => res.rows[0])
+    .then(first)
 }
 exports.addProperty = addProperty;
