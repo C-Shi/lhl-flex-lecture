2d35fa58783b3370eefdd15a96771233957584f7
C-Shi <cshi@ualberta.ca>
2022-07-12 22:05:57 -0600

    typo

--- module_7/component_based_react/cars/src/CarList.js
+++ module_7/component_based_react/cars/src/CarList.js
@@ -12,12 +12,12 @@ export default function CarList () {
     <>
       <h5>Model: {cars[0].model}</h5>
       <p>Make: {cars[0].make}</p>
-      <p>Price: ${cars[0].make}</p>
+      <p>Price: ${cars[0].price}</p>
       <p>Status: {cars[0].status}</p>
 
       <h5>Model: {cars[1].model}</h5>
       <p>Make: {cars[1].make}</p>
-      <p>Price: ${cars[1].make}</p>
+      <p>Price: ${cars[1].price}</p>
       <p>Status: {cars[1].status}</p>
 
     </>
