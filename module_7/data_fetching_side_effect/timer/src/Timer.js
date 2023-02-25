70ee45c46d724451bf55ddeb112b8770bec77571
C-Shi <cshi@ualberta.ca>
2023-02-25 16:49:12 -0700

    side effect start code

--- module_7/data_fetching_side_effect/timer/src/Timer.js
+++ module_7/data_fetching_side_effect/timer/src/Timer.js
@@ -4,15 +4,14 @@ export default function Timer() {
   const [time1, setTime1] = useState(0)
   const [time2, setTime2] = useState(100)
 
-
   return (
     <>
-      <h2>Timer1</h2>
-      <h1 id="timer1">{time1}</h1>
+      <h2>Time1</h2>
+      <h1 id="time1">{time1}</h1>
       <button onClick={() => setTime1(time1 + 1)}>Add One</button>
 
-      <h2>Timer2</h2>
-      <h1 id="timer2">{time2}</h1>
+      <h2>Time2</h2>
+      <h1 id="time2">{time2}</h1>
       <button onClick={() => setTime2(time2 - 1)}>Minus One</button>
     </>
   )
