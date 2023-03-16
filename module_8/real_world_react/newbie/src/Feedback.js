b821490eb38ffb335c74a3d3448889e83464f13d
Cheng <cshi@ualberta.ca>
2023-03-16 16:19:59 -0600

    using state

--- module_8/real_world_react/newbie/src/Feedback.js
+++ module_8/real_world_react/newbie/src/Feedback.js
@@ -1,32 +1,36 @@
 import { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 export default function Feedback(props) {
-  const [feedback, setFeedback] = useState('')
-  const textarea = useRef(null);
-
-  useEffect(() => {
-    textarea.current.focus()
-  }, [])
+  const [feedback, setFeedback] = useState({
+    name: '',
+    email: '',
+    comment: '',
+    rate: 0,
+  })
 
   let navigate = useNavigate()
 
-  useEffect(() => {
-    setFeedback(props.user ? `Hi, my name is ${props.user}.` : '')
-  }, [props.user])
-
-  const sendFeecback = (e) => {
-    e.preventDefault()
-    console.log(textarea.current.value)
-
-    // redirect to a different route after form is sent out
-    navigate("/")
-  }
 
   return (
-    <form style={{width: '60%', margin: '0 auto'}} onSubmit={sendFeecback}>
+    <form style={{width: '60%', margin: '0 auto'}}>
       <div className="form-group">
+        <label>Name</label>
+        <input className="form-control" value={feedback.name} onChange={e => { setFeedback({...feedback, name: e.target.value})}}></input>
+        
+        <label>Email</label>
+        <input className="form-control" value={feedback.email} onChange={e => { setFeedback({...feedback, email: e.target.value})}}></input>
+
         <label>Feedback</label>
-        <textarea ref={textarea} className="form-control" rows="3" value={feedback} onInput={(e) => setFeedback(e.target.value)}></textarea>
+        <textarea className="form-control" rows="3" value={feedback.comment} onChange={e => { setFeedback({...feedback, comment: e.target.value})}}></textarea>
+
+        <label>Rate</label>
+        <select className="form-control" value={feedback.star} onChange={e => { setFeedback({...feedback, rate: e.target.value})}}>
+          <option type="number">1</option>
+          <option type="number">2</option>
+          <option type="number">3</option>
+          <option type="number">4</option>
+          <option type="number">5</option>
+        </select>
       </div>
       <button type="submit" className="btn btn-default">Send Feedback</button>
     </form>
